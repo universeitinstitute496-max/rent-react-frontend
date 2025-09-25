e647553523f5dd0b872d2e6eff339ee959226f79
devsamin <abdullahalsamin79@gmail.com>
2025-09-25 02:47:56 -0700

    LoginComponent

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,58 +1,10 @@
-import { Link } from 'react-router'
+import LoginComponent from '../components/LoginComponent'
 
 const Login = () => {
     return (
-        <>
-            <>
-                {/* Login Form (Visible before login) */}
-                <div
-                    id="loginForm"
-                    className="max-w-md mx-auto bg-white rounded-lg shadow-md p-6 mt-10 mb-10"
-                >
-                    <h2 className="text-3xl font-bold text-center mb-6">Login</h2>
-                    <form>
-                        <div className="mb-4">
-                            <label className="block text-gray-700 mb-2">Email address</label>
-                            <input
-                                type="text"
-                                className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-                                placeholder="Enter your email address"
-                            />
-                        </div>
-                        <div className="mb-6">
-                            <label className="block text-gray-700 mb-2">Password</label>
-                            <input
-                                type="password"
-                                className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-                                placeholder="Enter your password"
-                            />
-                        </div>
-                        <p className="text-right mt-4 mb-4">
-                            <a
-                                href="forgot_password.html"
-                                className="text-blue-500 hover:underline"
-                            >
-                                Forgot password?
-                            </a>
-                        </p>
-                        <button
-                            type="button"
-                            id="loginSubmit"
-                            className="bg-blue-600 text-white w-full py-2 rounded-lg font-semibold hover:bg-blue-700"
-                        >
-                            Login
-                        </button>
-                    </form>
-                    <p className="text-center mt-4">
-                        If you don't have an account?{" "}
-                        <Link to="/registration/" className="text-blue-500 hover:underline">
-                            Register
-                        </Link>
-                    </p>
-                </div>
-            </>
-
-        </>
+       <div>
+        <LoginComponent/>
+       </div>
     )
 }
 
